fb259b839a2ce22ee1d9897522cfa6f1d5f2620d
kaweendras <salitha.wijerathna@gmail.com>
2024-11-08 14:27:51 +0530

    CHG | Initial setup

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+    allowedHeaders: [
+      "Access-Control-Allow-Origin",
+      "Access-Control-Allow-Headers",
+      "Content-Type",
+      "Origin",
+      "authorization",
+    ],
+  })
+);
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => console.log(`🗄️| Connected to MongoDB Databse successfully`))
+  .then(async () => {
+    app.listen(PORT);
+  })
+  .then(() => console.log(`🌎 | App Started on  http://localhost:${PORT}`))
+  .catch((err) => console.log("🚫 " + err));
