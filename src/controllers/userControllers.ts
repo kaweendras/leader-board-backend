60ac1588fda44880784e4d90e2e385ebccd3ed6a
kaweendras <salitha.wijerathna@gmail.com>
2024-11-08 16:35:19 +0530

    CHG | User Controller Changes

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -4,6 +4,7 @@ import {
   getAllUsers,
   loginUser,
   userProfiles,
+  updatePoints,
 } from "../services/userServices";
 
 import { verifyToken } from "../utils/authUtils";
@@ -40,6 +41,8 @@ const createUsersController = async (
 ) => {
   const userData = req.body;
   let user;
+  //append field points = 0 to the user data
+  userData.points = 0;
   try {
     user = await createUser(userData);
     if (user && user.success === "false") {
@@ -125,9 +128,94 @@ const getProfileController = async (
   }
 };
 
+const getUserByEmailController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  // const token = req.headers.authorization?.split(" ")[1];
+  // if (!token) {
+  //   return res.status(401).json({
+  //     success: "false",
+  //     message: "No token provided",
+  //   });
+  // }
+
+  // const { valid, decoded, error } = verifyToken(token);
+  // if (!valid) {
+  //   return res.status(401).json({
+  //     success: "false",
+  //     message: "Invalid token",
+  //     error,
+  //   });
+  // }
+
+  const email = req.query.email as string;
+  try {
+    const userProfile = await userProfiles(email);
+    if (userProfile.success === "false") {
+      return res.status(500).json({
+        success: "false",
+        message: userProfile.message,
+      });
+    }
+    return res.status(200).json({
+      success: "true",
+      data: userProfile.data,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      success: "false",
+      message: "Failed to retrieve profile",
+      data: [],
+    });
+  }
+};
+
+const updateUserPointsController = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({
+      success: "false",
+      message: "No token provided",
+    });
+  }
+
+  const email = req.body.email as string;
+
+  const points = req.body.points as number;
+  try {
+    const pointsRes = await updatePoints(email, points);
+    if (pointsRes.success === "false") {
+      return res.status(500).json({
+        success: "false",
+        message: pointsRes.message,
+      });
+    }
+    return res.status(200).json({
+      success: "true",
+      data: pointsRes.data,
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({
+      success: "false",
+      message: "Failed to retrieve profile",
+      data: [],
+    });
+  }
+};
+
 export {
   createUsersController,
   getAllUsersController,
   loginUserController,
   getProfileController,
+  getUserByEmailController,
+  updateUserPointsController,
 };
